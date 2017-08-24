fd438c90732e0142fd40fc43fb2676cbba2f7f88
Gymno <buddercreeperman@gmail.com>
2017-08-23 20:44:00 -0700

    pause/resume completed
    i'll probably now just ignore arthur for another *insert long amount of time* because that's what i do

---
+++ commands/music/pause.js
@@ -0,0 +1,24 @@
+exports.run = (message, a, s, d, permLevel) => {
+	if (!message.member.roles.find(r => r.name.toLowerCase() === 'dj') && !message.member.roles.find(r => r.name.toLowerCase() === 'music') && permLevel < 3) return message.channel.send('You do not have permission to pause music. To do so, get mod role or higher, or have the DJ or Music role');
+	if (!message.guild.music || !message.guild.music.queue) return message.channel.send('There is no music playing.');
+
+	if (message.guild.music.playing === false) return message.channel.send('The music is already paused. Use `' + d.config.prefix + 'resume` to resume playback.');
+
+	message.guild.voiceConnection.dispatcher.pause();
+	message.guild.music.playing = false;
+	message.channel.send('Music paused.');
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 2,
+	aliases: []
+};
+
+exports.help = {
+	name: 'Pause',
+	description: 'Pause the currently playing song',
+	usage: 'pause',
+	help: 'Pause the current song. Requires "DJ" or "Music" role (or Mod+)',
+	category: 'Music'
+};
---
+++ commands/music/resume.js
@@ -0,0 +1,24 @@
+exports.run = (message, a, s, d, permLevel) => {
+	if (!message.member.roles.find(r => r.name.toLowerCase() === 'dj') && !message.member.roles.find(r => r.name.toLowerCase() === 'music') && permLevel < 3) return message.channel.send('You do not have permission to resume music. To do so, get mod role or higher, or have the DJ or Music role');
+	if (!message.guild.music || !message.guild.music.queue) return message.channel.send('There is no music playing.');
+
+	if (message.guild.music.playing === true) return message.channel.send('The music is already playing. Use `' + d.config.prefix + 'pause` to pause playback.');
+
+	message.guild.voiceConnection.dispatcher.resume();
+	message.guild.music.playing = true;
+	message.channel.send('Music resumed.');
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 2,
+	aliases: [ 'go', 'continue' ]
+};
+
+exports.help = {
+	name: 'Resume',
+	description: 'Resume the currently playing song',
+	usage: 'resume',
+	help: 'Resume the current song. Requires "DJ" or "Music" role (or Mod+)',
+	category: 'Music'
+};
