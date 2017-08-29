78a31911c5a2bbe2183ab86f34d260e2a8e2a064
Gymno <buddercreeperman@gmail.com>
2017-08-28 19:23:25 -0700

    math command

---
+++ commands/apis/math.js
@@ -0,0 +1,37 @@
+const request = require('request');
+
+exports.run = (message, args, suffix, client) => {
+	if (!args[0]) return message.channel.send('Seriously? You expect me to evaluate nothing? :clap:');
+
+	request(`http://api.mathjs.org/v1/?expr=${encodeURIComponent(suffix)}&precision=3`, (err, response, body) => {
+		if (err) return message.channel.send('Your equation is invalid. Rude.');
+		if (body.toLowerCase().startsWith('error')) {
+			message.channel.send({embed: {
+				title: suffix,
+				description: `\`\`\`js\n${body}\n\`\`\``,
+				color: 0xff3d3d
+			}});
+		} else {
+			message.channel.send({embed: {
+				title: suffix,
+				description: `\`\`\`js\n${body}\n\`\`\``,
+				color: 0x70ff3d
+			}});
+		}
+	});
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 2,
+	aliases: ['calc'],
+	cooldown: 2000
+};
+
+exports.help = {
+	name: 'Math',
+	description: 'Evaluate math expressions and conversions.',
+	usage: 'math <expression>',
+	help: 'Evaluate math expressions or conversions.',
+	category: 'APIs'
+};
