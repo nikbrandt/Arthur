3d01ae635f3a9bd85f1e3130a61e9e470d3b27d5
Gymno <buddercreeperman@gmail.com>
2017-10-03 22:09:46 -0700

    stopwatch command

---
+++ commands/fun/stopwatch.js
@@ -0,0 +1,37 @@
+const Stopwatch = require('timer-stopwatch');
+const UserObject = {};
+
+function secSpread(sec) {
+	let hours = Math.floor(sec / 3600);
+	let mins = Math.floor((sec - hours * 3600) / 60);
+	let secs = sec - (hours * 3600 + mins * 60);
+	return `${hours ? `${hours}h ` : ''}${mins ? `${mins}m ` : ''}${secs ? `${secs}s` : ''}`;
+}
+
+exports.run = message => {
+	if (!UserObject[message.author.id]) UserObject[message.author.id] = new Stopwatch();
+
+	let on = UserObject[message.author.id].startstop();
+
+	if (on) {
+		UserObject[message.author.id].reset();
+		UserObject[message.author.id].start();
+		message.channel.send('Stopwatch started.');
+	} else {
+		message.channel.send(`Stopwatch stopped. ${secSpread(Math.ceil(UserObject[message.author.id].ms / 1000))}.`);
+	}
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 2,
+	aliases: []
+};
+
+exports.help = {
+	name: 'Stopwatch',
+	description: 'Time something with your dandy stopwatch',
+	usage: 'stopwatch',
+	help: 'Start or stop your stopwatch.',
+	category: 'Fun'
+};
