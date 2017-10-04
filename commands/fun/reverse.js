ef71b696d7e6c05c5961e4f0b9cef85318653867
Gymno <buddercreeperman@gmail.com>
2017-10-03 21:41:48 -0700

    reverse command

---
+++ commands/fun/reverse.js
@@ -0,0 +1,24 @@
+function reverse(text) {
+	let array = text.split('');
+	array.reverse();
+	return array.join('');
+}
+
+exports.run = (message, args, suffix) => {
+	if (!args[0]) return message.channel.send('You\'ve gotta provide me with something to reverse, lul');
+	message.channel.send(reverse(suffix));
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 2,
+	aliases: ['flip']
+};
+
+exports.help = {
+	name: 'Reverse',
+	description: 'Reverse some text',
+	usage: 'reverse <text>',
+	help: 'Reverse some text. Might be entertaining, likely won\'t be.',
+	category: 'Fun'
+};
