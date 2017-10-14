8083c799918fdb60d6473ed8e677983e7cd4c68d
Gymno <buddercreeperman@gmail.com>
2017-10-14 15:32:23 -0700

    add ID command

---
+++ commands/developer/id.js
@@ -0,0 +1,30 @@
+exports.run = (message, args, suffix, client, permLevel) => {
+	if (!args[0]) return message.channel.send('How am I going to get the ID of no one? Ey?');
+
+	let user;
+	if (permLevel === 10) user = client.users.find('tag', suffix) || client.users.find(u => u.tag.toLowerCase().includes(suffix.toLowerCase()));
+	else user = message.guild.members.find(m => m.user.tag.toLowerCase() === suffix.toLowerCase()) || message.guild.members.find(m => m.user.tag.toLowerCase().includes(suffix.toLowerCase()));
+
+	if (!user) return message.channel.send('I don\'t have access to that user or they don\'t exist.. Sorry!');
+
+	message.channel.send({embed: {
+		title: user.id,
+		footer: {
+			text: `ID of ${user.username}`
+		}
+	}});
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 1,
+	aliases: []
+};
+
+exports.help = {
+	name: 'ID',
+	description: 'Get a user\'s Discord Snowflake ID',
+	usage: 'id <partial or full Discord tag>',
+	help: 'Get a user\'s Discord Snowflake ID.',
+	category: 'Developer'
+};
