bf72dc034431ed8f837a8fe2181e168ea8ca4c55
Gymno <buddercreeperman@gmail.com>
2017-10-14 14:29:34 -0700

    make bot leave voice if only bots in channel

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,6 +1,6 @@
 module.exports = (client, old, member) => {
 	setTimeout(() => {
-		if (member.guild.voiceConnection && member.guild.voiceConnection.channel.members.size === 1) {
+		if (member.guild.voiceConnection && (member.guild.voiceConnection.channel.members.size === 1 || !member.guild.voiceConnection.channel.members.exists(m => m.user.bot === true))) {
 			member.guild.music = {};
 			member.guild.voiceConnection.disconnect();
 		}
