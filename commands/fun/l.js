62ef33cf6d85b14f7e6cc4f142929488afc6bf9c
Gymno <buddercreeperman@gmail.com>
2017-10-21 20:26:58 -0700

    create L command

---
+++ commands/fun/l.js
@@ -0,0 +1,17 @@
+exports.run = message => {
+	message.channel.send(':regional_indicator_l:\n:regional_indicator_l:\n:regional_indicator_l:\n:regional_indicator_l::regional_indicator_l::regional_indicator_l:');
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 2,
+	aliases: []
+};
+
+exports.help = {
+	name: 'L',
+	description: 'L',
+	usage: 'L',
+	help: 'Post an L',
+	category: 'Fun'
+};
