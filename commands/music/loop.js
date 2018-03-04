054de6a96638f17c741b022b643f6b51db7cbd76
Gymno <buddercreeperman@gmail.com>
2018-03-03 16:47:10 -0800

    elaborate loop permissions + add some

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -1,13 +1,13 @@
 exports.run = (message, a, s, c, permLevel) => {
 	if (!message.guild.music || !message.guild.music.queue) return message.channel.send('Ah yes, I\' make sure to loop the nonexistent music. (play some music thanks)');
-	if (!message.member.roles.find(r => r.name.toLowerCase() === 'dj') && !message.member.roles.find(r => r.name.toLowerCase() === 'music') && permLevel < 3) return;
+	if (!message.member.roles.find(r => r.name.toLowerCase() === 'dj') && !message.member.roles.find(r => r.name.toLowerCase() === 'music' && message.guild.music.queue[0].person.id !== message.author.id) && permLevel < 3) return message.channel.send('hey you don\'t have permission to loop the music, weirdo');
 
 	if (message.guild.music.loop) {
 		message.guild.music.loop = false;
 		message.channel.send('A\'ight loops off, as you wish.');
 	} else {
 		message.guild.music.loop = true;
-		message.channel.send('Loops on, have fun listening to the same thing *forever*');
+		message.channel.send('Loops on, have fun listening to the same stuff *forever*');
 	}
 };
 
