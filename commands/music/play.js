5979df8c411f692129b4ca9191f664471d2fa4a8
Gymno <buddercreeperman@gmail.com>
2018-04-26 19:11:56 -0700

    update play command usage

--- commands/music/play.js
+++ commands/music/play.js
@@ -114,7 +114,7 @@ exports.config = {
 exports.help = {
 	name: 'Play',
 	description: 'Play a song or add it to the queue',
-	usage: 'play <song name/youtube url/file url/file/top/liked> [extra params]',
+	usage: 'play <song name/youtube or soundcloud url/file url/file/top/liked> [extra params]',
 	help: 'Play a song or add one to the queue. Only supports songs playable in the US.',
 	category: 'Music'
 };
