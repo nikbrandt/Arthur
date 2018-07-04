be8e5b8739e525e81213dbe8a694ebeb0daefe81
Gymno <buddercreeperman@gmail.com>
2018-07-04 01:11:41 -0700

    add spotifyusers
    but never implement it because it doesn't work well with how dicksword works

---
+++ commands/misc/spotifyusers.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const util = require('util');
+
+exports.run = (message, args, suffix, client) => {
+	let number = 89;
+	let users = new Discord.Collection();
+
+	if (args[0]) {
+		let tempNumber = parseInt(args[0]);
+		if (tempNumber) number = tempNumber;
+		if (tempNumber > 89) number = 89;
+		if (tempNumber < 1) number = 1;
+	}
+
+	client.guilds.filter(guild => {
+		return guild.members.has(message.author.id);
+	}).forEach(guild => {
+		users = guild.members.concat(users);
+	});
+
+	users = users.filter(member => {
+		if (member.presence.game && member.presence.game.name === 'Spotify') console.log(' - with game ', member.presence.game);
+		return member.presence.game && member.presence.game.name === 'Spotify';
+	});
+
+	users = users.random(number);
+
+	message.channel.send( { embed: {
+		color: 0x00c140,
+		title: `${number} random users playing Spotify`,
+		description: users.map(user => user.user.toString()).join(' ')
+	} } );
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 9,
+	aliases: [ 'spotify', 'spusers', 'susers', 'spotifys', 'spotifies', 'musicusers' ],
+	perms: [ 'EMBED_LINKS' ]
+};
+
+exports.help = {
+	name: 'Spotify Users',
+	description: 'Get a list of *x* users currently listening to Spotify.',
+	usage: 'spotifyusers [amount]',
+	help: 'Get a list of `amount` users currently listening to Spotify. Default 20. Users must share a server with you; if you\'re not seeing many, try joining one or both of the Discord Bots servers.',
+	category: 'Developer'
+};
