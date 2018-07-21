7a4b993446f5aeae6c04bac1bb33c8fdf7be2ba9
Gymnophoria <buddercreeperman@gmail.com>
2018-07-21 14:51:43 -0700

    add raw message reaction events

--- functions/eventLoader.js
+++ functions/eventLoader.js
@@ -1,4 +1,3 @@
-const { Stopwatch } = require('node-stopwatch');
 const Discord = require('discord.js');
 const config = require('../../media/config.json');
 const statusWebhookClient = new Discord.WebhookClient(config.statusLog.id, config.statusLog.token);
@@ -18,17 +17,21 @@ function statusUpdate (embed, restart, client) {
 			});
 		}, 60000);
 	}).catch(console.error);
-};
+}
 
 const errorLog = (error, stack, code) => {
 	if (!(process.argv[2] && process.argv[2] === 'test')) errorWebhookClient.send({ embeds: [ { title: error, description: stack, footer: { text: `Code ${code}` }, timestamp: new Date().toISOString(), color: 0xff0000 } ] }).catch(console.error);
 };
 
+const rawEvents = {
+	MESSAGE_REACTION_ADD: 'messageReactionAdd',
+	MSESAGE_REACTION_REMOVE: 'messageReactionRemove'
+};
+
 exports.load = client => {
-	let stopwatch = Stopwatch.create();
+	let start = Date.now();
 	let events = fs.readdirSync('./events');
 	console.log(`Loading ${events.length} events..`);
-	stopwatch.start();
 
 	events.forEach(file => {
 		console.log(`Loading ${file}..`);
@@ -39,8 +42,24 @@ exports.load = client => {
 		console.log('Loaded.');
 	});
 
-	console.log(`Loaded ${events.length} events in ${stopwatch.elapsedMilliseconds} ms.\n`);
-	stopwatch.stop();
+	console.log(`Loaded ${events.length} events in ${Date.now() - start} ms.\n`);
+	
+	client.on('raw', async event => {
+		if (!rawEvents.hasOwnProperty(event.t)) return;
+		
+		const { d: data } = event;
+		const user = client.users.get(data.user_id); // or client.fetchUser when fetchAllMembers is off
+		const channel = client.channels.get(data.channel_id); // || await user.createDM(); if using DM reactions
+		
+		if (!channel || channel.messages.has(data.message_id)) return;
+		
+		const message = await channel.fetchMessage(data.message_id);
+
+		const emojiKey = (data.emoji.id) ? `${data.emoji.name}:${data.emoji.id}` : data.emoji.name;
+		const reaction = message.reactions.get(emojiKey);
+
+		client.emit(rawEvents[event.t], reaction, user);
+	});
 
 	client.on('debug', d => {
 		if (d.includes('Session invalidated')) statusUpdate({
