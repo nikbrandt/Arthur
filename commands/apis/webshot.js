8272df244728276e719ddefbe597650c2e3e6ee6
Gymnophoria <buddercreeperman@gmail.com>
2018-08-03 13:05:57 -0700

    theoretical webshot ip address hiding + more filtering

--- commands/apis/webshot.js
+++ commands/apis/webshot.js
@@ -1,13 +1,15 @@
+const requestPromise = require('request-promise');
 const Discord = require('discord.js');
 const webshot = require('webshot');
 const moment = require('moment');
 const fs = require('fs');
 
-let nonobad = ['porn', 'redtube', 'sex'];
+let nonobad = ['porn', 'redtube', 'sex', 'rule34', 'anal', 'amateur', 'cuckold', 'creampie', 'cum', 'jiz', 'milf', 'orgasm', 'orgy', 'threesome', 'ass', 'tit', 'dick', 'penis', 'despacito', 'pussy', 'fuck', 'finger', 'bang', 'hentai', 'yaoi', 'virgin', 'handjob', 'blowjob', 'xxx', 'milf'];
+const ipAddressRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/g;
 
-exports.run = (message, args, s, client) => {
+exports.run = async (message, args, s, client) => {
 	if (!args[0]) return message.channel.send('You have to tell me what to take a picture of..');
-	if (nonobad.some(i => args[0].includes(i))) return message.channel.send('please..');
+	if (nonobad.some(i => args[0].toLowerCase().includes(i))) return message.channel.send('please..');
 	let index = client.processing.length;
 	client.processing.push(moment().format('h:mm:ss A') + ' - Webshot');
 	let date = Date.now();
@@ -22,7 +24,6 @@ exports.run = (message, args, s, client) => {
 			width: 'window',
 			height: 'all'
 		},
-		cookies: [],
 		timeout: 30000
 	};
 
@@ -35,6 +36,17 @@ exports.run = (message, args, s, client) => {
 		else msg = m;
 	});
 
+	let body;
+	
+	try {
+		body = await requestPromise(args[0]);
+	} catch (e) {
+		return message.channel.send('Hey, you gotta provide me with a *valid* url, okay? Your trickery caused you a 10 second cooldown, mister.');
+	}
+	
+	if (!body) return message.channel.send('Hey, you gotta provide me with a *valid* url, okay? Your trickery caused you a 10 second cooldown, mister.');
+	body = body.replace(ipAddressRegex, 'ip address hidden, nerd');
+	
 	setTimeout(function () {
 		if (!sent) {
 			message.channel.send('That website is too powerful! It\'s taken me more than 30 seconds to render, so I\'m canceling the render. Sorry!');
@@ -43,7 +55,7 @@ exports.run = (message, args, s, client) => {
 		}
 	}, 35000); // if webshot not complete in 35 seconds, cancel operation.
 
-	webshot(args[0], `../media/temp/${date}-${message.author.id}.png`, options, err => {
+	webshot(body, `../media/temp/${date}-${message.author.id}.png`, options, err => {
 		if (cancel) return;
 
 		if (err) {
