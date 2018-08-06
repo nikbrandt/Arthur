414695c660e7bde2e6226ef15ae92ab0d1558a43
Gymnophoria <buddercreeperman@gmail.com>
2018-08-06 00:46:11 -0700

    make pull better discreetKappa

--- commands/developer/pull.js
+++ commands/developer/pull.js
@@ -2,15 +2,18 @@ const { exec } = require('child_process');
 
 exports.run = (message, args, suffix, client) => {
 	let reload = false;
+	let kys = false;
 	if (args[0] === '-r') reload = true;
+	if (args[0] === '-k' || args[0] === '-kys') kys = true;
 	
 	exec('git pull', (error, stdout) => {
 		if (error) return message.channel.send(`Received error: \`\`\`shell\n${error.stack}\`\`\``);
 		
 		if (stdout.includes('up to date.')) return message.channel.send('Already up to date.');
 		if (stdout.includes('changed, ')) {
-			message.channel.send(`Pulled successfully <:discreetkappa:474445143974608898> ${reload ? '- Reloading commands now.' : ''}`);
-			if (reload) client.commands.get('load').run(message, args, suffix, client);
+			message.channel.send(`Pulled successfully <:discreetkappa:474445143974608898> ${kys ? '- Killing self now.' : ''}${reload ? '- Reloading commands now.' : ''}`);
+			if (kys) client.commands.get('restart').run(message, args, suffix, client);
+			else if (reload) client.commands.get('load').run(message, args, suffix, client);
 			return;
 		}
 		
