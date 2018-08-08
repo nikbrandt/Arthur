9f50c592d3dbadad536747e137a24eddc944cde8
Gymnophoria <buddercreeperman@gmail.com>
2018-08-08 09:50:42 -0700

    change already_playing to already_paused in pause

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -2,7 +2,7 @@ exports.run = (message, a, s, d, permLevel) => {
 	if (!message.member.roles.find(r => r.name.toLowerCase() === 'dj') && !message.member.roles.find(r => r.name.toLowerCase() === 'music') && permLevel < 3) return message.react(':missingpermissions:407054344874229760').catch(() => {});
 	if (!message.guild.music || !message.guild.music.queue) return message.channel.send(message.__('no_music_playing'));
 
-	if (message.guild.music.playing === false) return message.channel.send(message.__('already_playing'));
+	if (message.guild.music.playing === false) return message.channel.send(message.__('already_paused'));
 
 	message.guild.voiceConnection.dispatcher.pause();
 	message.guild.music.playing = false;
