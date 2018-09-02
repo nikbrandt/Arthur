2f3ba87b940fac8eeda3e498a44d3d9b0aa0abf4
Gymnophoria <buddercreeperman@gmail.com>
2018-09-02 00:37:58 -0700

    proper npnotify usageifying grammar yes kill me

--- commands/music/npnotify.js
+++ commands/music/npnotify.js
@@ -3,8 +3,10 @@ const sql = require('sqlite');
 exports.run = async (message) => { // npNotify column
 	let row = await sql.get(`SELECT npNotify FROM guildOptions WHERE guildID = '${message.guild.id}'`);
 
-	if (!row) row = 'false';
-	else row = row.npNotify;
+	if (!row) {
+		row = 'false';
+		await sql.run(`INSERT INTO guildOptions (guildID) VALUES (?)`, [ message.guild.id ]);
+	} else row = row.npNotify;
 
 	if (row === 'false') {
 		message.channel.send(message.__('message', { status: message.__('enabled') }));
