15bb393e5a06fdc59e98fdd034f073a423a1c780
Gymnophoria <buddercreeperman@gmail.com>
2018-09-30 01:24:52 -0700

    never testing code is best testing code wait what fuck scopes man

--- commands/apis/dog.js
+++ commands/apis/dog.js
@@ -3,11 +3,15 @@ const request = require('request');
 exports.run = message => {
 	request('http://dog.ceo/api/breeds/image/random', (err, response, body) => {
 		if (err) return message.channel.send(message.__('error'));
+		let json;
 		try {
-			const json = JSON.parse(body);
+			json = JSON.parse(body);
 		} catch (e) {
 			return message.channel.send(message.__('error'));
 		}
+		
+		if (!json) return message.channel.send(message.__('error'));
+		
 		message.channel.send({embed: {
 			description: message.__('embed_description', { url: json.message }),
 			image: {
