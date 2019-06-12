783d02479d55d20412071ad82672c2d76409e014
Gymno <buddercreeperman@gmail.com>
2019-06-12 15:35:47 -0700

    deal with d.js deprecations
    Collection#find with 'prop', 'value' in favor of a function passed in and Collection#exists in favor of Collection#some

--- commands/developer/id.js
+++ commands/developer/id.js
@@ -2,7 +2,7 @@ exports.run = (message, args, suffix, client, permLevel) => {
 	if (!args[0]) return message.channel.send('How am I going to get the ID of no one? Ey?');
 
 	let user;
-	if (permLevel === 10) user = client.users.find('tag', suffix) || client.users.find(u => u.tag.toLowerCase().includes(suffix.toLowerCase()));
+	if (permLevel === 10) user = client.users.find(u => u.tag === suffix) || client.users.find(u => u.tag.toLowerCase().includes(suffix.toLowerCase()));
 	else user = message.guild.members.find(m => m.user.tag.toLowerCase() === suffix.toLowerCase()) || message.guild.members.find(m => m.user.tag.toLowerCase().includes(suffix.toLowerCase()));
 
 	if (!user) return message.channel.send(message.__('user_does_not_exist'));
--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,6 +1,6 @@
 module.exports = (client, old, member) => {
 	setTimeout(() => {
-		if (member.guild.voiceConnection && (member.guild.voiceConnection.channel.members.size === 1 || !member.guild.voiceConnection.channel.members.exists(m => m.user.bot === true))) {
+		if (member.guild.voiceConnection && (member.guild.voiceConnection.channel.members.size === 1 || !member.guild.voiceConnection.channel.members.some(m => m.user.bot === false))) {
 			member.guild.music = {};
 			member.guild.voiceConnection.disconnect();
 		}
