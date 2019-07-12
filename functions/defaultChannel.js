240d91fe0b671e1c1115ee35c7fcc40c631f594c
GlitchMasta47 <glitchmasta47@hotmail.com>
2019-07-11 21:25:49 -0400

    slight size reductions
    'n stuff

--- functions/defaultChannel.js
+++ functions/defaultChannel.js
@@ -1,12 +1,8 @@
 module.exports = guild => {
-	let channel;
-
 	let textChannels = guild.channels.filter(g => g.type === 'text');
 	if (textChannels.size < 1) return undefined;
-
-	channel = textChannels.find(c => c.name === 'general');
-	if (!channel) channel = textChannels.get(guild.id);
-	if (!channel) channel = textChannels.first();
-
-	return channel;
+	
+	return textChannels.find(c => c.name === 'general') ||
+		textChannels.get(guild.id) ||
+		textChannels.first();
 };
