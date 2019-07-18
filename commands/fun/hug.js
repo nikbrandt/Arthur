d29adf4a1a1a6145629a5b7c0395b15444e49be7
Gymnophoria <buddercreeperman@gmail.com>
2019-07-17 18:40:05 -0700

    refactor hug in hopes that it might work on the debian meme machine
    damn that was a beautiful refactor

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -1,77 +1,35 @@
-const { createCanvas, Image } = require('canvas');
-const fs = require('fs');
-const request = require('request');
+const { createCanvas, loadImage } = require('canvas');
 
-exports.run = (message, args, suffix, client) => {
-	let receiverURL;
+exports.run = async (message, args, suffix, client) => {
+	let receiverURL = '../media/images/hugArthur.png';
 
 	if (args[0]) {
 		let obj = client.findMember(message, suffix);
-		if (!obj) receiverURL = 'https://i.imgur.com/PO6OvAH.png'; // arthur pfp but specialized
-		else receiverURL = obj.user.displayAvatarURL;
-	} else receiverURL = 'https://i.imgur.com/PO6OvAH.png';
-
-	const canvas = createCanvas(300, 350),
-		ctx = canvas.getContext('2d');
-
-	let counter = 0;
-	let backgroundImageFile;
-	let receiverImageFile;
-	let giverImageFile;
-
-	request.get(receiverURL)
-		.pipe(fs.createWriteStream(`../media/temp/r${message.id}.png`))
-		.on('finish', () => {
-			fs.readFile(`../media/temp/r${message.id}.png`, (err, image) => {
-				receiverImageFile = image;
-				counter++;
-				complete();
-			})
-		});
-
-	request.get(message.author.displayAvatarURL)
-		.pipe(fs.createWriteStream(`../media/temp/g${message.id}.png`))
-		.on('finish', () => {
-			fs.readFile(`../media/temp/g${message.id}.png`, (err, image) => {
-				giverImageFile = image;
-				counter++;
-				complete();
-			})
-		});
-
-	fs.readFile('../media/images/hug.png', (err, image) => {
-		backgroundImageFile = image;
-		counter++;
-		complete();
-	});
-
-	function complete() {
-		if (counter !== 3) return;
-
-		let backgroundImage = new Image();
-		backgroundImage.src = backgroundImageFile;
-		ctx.drawImage(backgroundImage, 0, 0);
-
-		let receiverImage = new Image();
-		receiverImage.src = receiverImageFile;
-		ctx.save();
-		ctx.translate(35, 20);
-		ctx.rotate(-8 * Math.PI / 180);
-		ctx.drawImage(receiverImage, 0, 0, 110, 110);
-		ctx.restore();
-
-		let giverImage = new Image();
-		giverImage.src = giverImageFile;
-		ctx.save();
-		ctx.translate(155, 15);
-		ctx.rotate(6 * Math.PI / 180);
-		ctx.drawImage(giverImage, 0, 0, 110, 110);
-
-		fs.unlinkSync(`../media/temp/r${message.id}.png`);
-		fs.unlinkSync(`../media/temp/g${message.id}.png`);
-
-		message.channel.send({ files: [{ attachment: canvas.toBuffer(), name: 'hug.png' }] });
+		if (obj) receiverURL = obj.user.displayAvatarURL;
 	}
+
+	const canvas = createCanvas(300, 350);
+	const ctx = canvas.getContext('2d');
+
+	let [ receiverImage, giverImage, backgroundImage ] = await Promise.all([
+		loadImage(receiverURL),
+		loadImage(message.author.displayAvatarURL),
+		loadImage('../media/images/hug.png')
+	]);
+	
+	ctx.drawImage(backgroundImage, 0, 0);
+
+	ctx.save();
+	ctx.translate(35, 20);
+	ctx.rotate(-8 * Math.PI / 180);
+	ctx.drawImage(receiverImage, 0, 0, 110, 110);
+	ctx.restore();
+	
+	ctx.translate(155, 15);
+	ctx.rotate(6 * Math.PI / 180);
+	ctx.drawImage(giverImage, 0, 0, 110, 110);
+
+	message.channel.send({ files: [{ attachment: canvas.toBuffer(), name: 'hug.png' }] });
 };
 
 exports.config = {
