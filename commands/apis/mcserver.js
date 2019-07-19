6cca91ce29d171de66c959bc07241fd27c192f60
Gymnophoria <buddercreeperman@gmail.com>
2019-07-18 18:00:30 -0700

    fix mcserver
    jesus.

--- commands/apis/mcserver.js
+++ commands/apis/mcserver.js
@@ -1,8 +1,19 @@
 const request = require('request');
 const webshot = require('webshot');
-const Discord = require('discord.js');
+const { RichEmbed } = require('discord.js');
 const moment = require('moment');
 const fs = require('fs');
+
+const dataRegex = /^data:image\/([a-z]+);base64,/;
+const { errorLog } = require('../../functions/eventLoader');
+
+const customCSS = `
+@font-face { font-family: 'Minecraftia'; src: url('../media/fonts/Minecraft-Regular.ttf') format('truetype'); font-weight: normal; font-style: normal; }
+@font-face { font-family: 'Minecraftia'; src: url('../media/fonts/Minecraft-Italic.ttf') format('truetype'); font-weight: normal; font-style: italic; }
+@font-face { font-family: 'Minecraftia'; src: url('../media/fonts/Minecraft-Bold.ttf') format('truetype'); font-weight: bold; font-style: normal; }
+@font-face { font-family: 'Minecraftia'; src: url('../media/fonts/Minecraft-BoldItalic.ttf') format('truetype'); font-weight: bold; font-style: italic; }
+* { font-size: 24px; font-family: Minecraftia, Minecraft, sans-serif }
+`;
 
 function failed (messageOptions, msg, client, index) {
 	msg.edit('', messageOptions);
@@ -23,37 +34,61 @@ exports.run = async (message, args, s, client) => {
 	let index = client.processing.length;
 	client.processing.push(moment().format('h:mm:ss A') + ' - MC Server/Webshot');
 	let msg = await message.channel.send(message.__('processing'));
+	
+	let indexOfColon = args[0].indexOf(':');
+	let port = '';
+	let domain = args[0];
+	if (indexOfColon >= 0) {
+		port = `/${args[0].substring(indexOfColon + 1)}`;
+		domain = args[0].substring(0, indexOfColon);
+	}
+	
+	request(`https://api.mcsrvstat.us/2/${domain}${port}`, (err, response, body) => {
+		if (err || !body) return failed({embed: failedEmbed}, msg, client, index);
+		
+		try {
+			body = JSON.parse(body);
+		} catch (e) {
+			return failed({ embed: failedEmbed }, msg, client, index);
+		}
 
-	request(`https://use.gameapis.net/mc/query/info/${args[0]}`, (err, response, body) => {
-		if (!body || body.startsWith('<html')) return failed({embed: failedEmbed}, msg, client, index);
+		if (!body.online) return failed({embed: failedEmbed}, msg, client, index);
+		
+		let fileLocation = `../media/temp/${message.id}.png`;
+		let html = body.motd.html.join('<br>').replace(/ {2}/g, '&nbsp;&nbsp;');
 
-		let json = JSON.parse(body);
-		let date = Date.now();
-		let num = Math.floor(Math.random() * 10000);
-
-		if (!json.status) return failed({embed: failedEmbed}, msg, client, index);
-
-		webshot(json.motds.html.replace('\n', '<br>'), `../media/temp/${num}-${date}.png`, {siteType: 'html', windowSize: { width: 700, height: 85 }, customCSS: '@font-face{font-family:Minecraftia;src:url(../media/fonts/Minecraftia-Regular.ttf) format(\'truetype\')}*{font-size:24px;font-family:Minecraftia}'}, err => {
-			const embed = new Discord.RichEmbed()
-				.setAuthor(message.__('embed.title', { hostname: json.hostname }), `https://use.gameapis.net/mc/query/icon/${args[0]}`)
-				.setFooter(message.__('embed.footer', { port: json.port, ping: json.ping, protocol: json.protocol }))
-				.addField(message.__('embed.players'), `${json.players.online}/${json.players.max}`, true)
-				.addField(message.__('embed.version'), json.version.replace(/§./g, ''), true)
+		webshot(html, fileLocation, {siteType: 'html', windowSize: { width: 700, height: 85 }, customCSS }, err => {
+			let iconBase64 = body.icon;
+			let iconFilename = 'icon.' + iconBase64.match(dataRegex)[1];
+			iconBase64 = iconBase64.replace(dataRegex, '');
+			let iconBuffer = Buffer.from(iconBase64, 'base64');
+			
+			let files = [ { attachment: iconBuffer, name: iconFilename } ];
+			
+			const embed = new RichEmbed()
+				.setAuthor(message.__('embed.title', { hostname: body.hostname }), `attachment://${iconFilename}`)
+				.setFooter(message.__('embed.footer', { port: body.port, protocol: body.protocol }))
+				.addField(message.__('embed.players'), `${body.players.online}/${body.players.max}`, true)
+				.addField(message.__('embed.version'), body.version.replace(/§./g, ''), true)
 				.setColor(0x00c140);
 			
 			if (err) {
 				console.log('mcserver command error while rendering webshot - rip\n', err);
-				embed.setDescription('```\n' + json.motds.clean + '```');
+				errorLog('mcserver command failed while getting webshot', err.stack, err.code);
+				embed.setDescription('```\n' + body.motd.clean.join('\n') + '```');
 			} else {
-				embed.attachFile(`../media/temp/${num}-${date}.png`)
-					.setImage(`attachment://${num}-${date}.png`);
+				files.push({ attachment: fileLocation, name: 'motd.png' });
+				
+				embed.setImage(`attachment://motd.png`);
 			}
+			
+			embed.attachFiles(files);
 
 			message.channel.send({embed}).then(() => {
 				msg.delete().catch(() => {});
 				try {
 					client.processing.splice(index, 1);
-					fs.unlinkSync(`../media/temp/${num}-${date}.png`);
+					fs.unlinkSync(fileLocation);
 				} catch (e) {}
 			});
 		})
@@ -61,7 +96,7 @@ exports.run = async (message, args, s, client) => {
 };
 
 exports.config = {
-	enabled: false,
+	enabled: true,
 	permLevel: 1,
 	perms: [ 'EMBED_LINKS', 'ATTACH_FILES' ],
 	category: 'apis'
