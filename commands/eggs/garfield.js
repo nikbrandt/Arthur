b9a2046254fa8416e484f1f031edd6ec2eb85241
GlitchMasta47 <17860556+GlitchMasta47@users.noreply.github.com>
2020-02-17 19:49:15 -0600

    Garfield easter egg (suggestion)
    Suggested by sooner at https://trello.com/c/2TdHnySs/577-agarfield-everytime-someone-says-this-you-post-a-random-picture-of-garfield-this-is-for-us-garfield-fans and https://trello.com/c/YSNciHVB/587-so-listen-i-feel-like-my-suggestions-are-getting-lost-somewhere-agarfield-would-be-an-excellent-addition-to-the-arthur-bot-if-yo
    This should be the only commit this file ever has, if not I will be very happyn't

---
+++ commands/eggs/garfield.js
@@ -0,0 +1,18 @@
+exports.run = (message) => {
+	message.channel.send({files: ['https://i.imgur.com/PhY66kU.png']});
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 1,
+	perms: ['ATTACH_FILES'],
+	category: 'eggs'
+};
+
+exports.meta = {
+	command: 'garfield',
+	name: 'garfield',
+	description: 'An easter egg based off garfield',
+	usage: 'garfield',
+	help: 'An easter egg based off garfield'
+};
