660df4c3d6bbda1258d989154873b0033a92ba0c
Gymnophoria <buddercreeperman@gmail.com>
2020-07-07 03:58:15 -0700

    Add spongebob and spacify commands
    These are very important commands, y'know? Gotta have those fun commands.

---
+++ commands/fun/spacify.js
@@ -0,0 +1,34 @@
+function spacify(text) {
+	let array = text.split('');
+	let n = text.length;
+	let out = '';
+
+	for (let i = 0; i < n - 1; i++) {
+		if (array[i] === ' ') out += '   ';
+		else out += array[i] + ' ';
+	}
+	
+	out += array[n - 1];
+
+	return out;
+}
+
+exports.run = (message, args, suffix) => {
+	if (!args[0]) return message.channel.send(message.__('no_args'));
+	
+	let desc = '```' + spacify(suffix) + '```';
+	if (desc.length > 2048) return message.channel.send(message.__('too_big'));
+
+	message.channel.send({
+		embed: {
+			description: '```' + spacify(suffix) + '```',
+			color: 0x00c140
+		}
+	});
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 1,
+	category: 'fun'
+};
---
+++ commands/fun/spongebob.js
@@ -0,0 +1,37 @@
+function alternateCaps(text) {
+	let array = text.split('');
+	let n = text.length;
+	let out = '';
+	let caps = false;
+	
+	for (let i = 0; i < n; i++) {
+		if (!/[A-Za-z]/.test(array[i])) {
+			out += array[i];
+			continue;
+		}
+		
+		if (caps) out += array[i].toUpperCase();
+		else out += array[i].toLowerCase();
+		
+		caps = !caps;
+	}
+	
+	return out;
+}
+
+exports.run = (message, args, suffix) => {
+	if (!args[0]) return message.channel.send(message.__('no_args'));
+	
+	message.channel.send({
+		embed: {
+			description: alternateCaps(suffix),
+			color: 0x00c140
+		}
+	});
+};
+
+exports.config = {
+	enabled: true,
+	permLevel: 1,
+	category: 'fun'
+};
