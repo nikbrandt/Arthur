59bea0325bb50b5b1f4d3966d30ff696c401d601
Gymno <buddercreeperman@gmail.com>
2020-12-05 21:01:38 -0800

    make isaac HD-er

--- commands/eggs/isaac.js
+++ commands/eggs/isaac.js
@@ -1,7 +1,7 @@
 const { createCanvas, loadImage } = require('canvas');
 
 exports.run = async (message, args, suffix, client) => {
-	const canvas = createCanvas(250, 250);
+	const canvas = createCanvas(2048, 2048);
 	const ctx = canvas.getContext('2d');
 	
 	let userImage;
@@ -9,14 +9,14 @@ exports.run = async (message, args, suffix, client) => {
 	if (obj) userImage = obj.user;
 	else userImage = message.author;
 	
-	userImage = await loadImage(userImage.displayAvatarURL({ format: 'png' }));
+	userImage = await loadImage(userImage.displayAvatarURL({ format: 'png', size: 2048 }));
 	
-	ctx.drawImage(userImage, 0, 0, 250, 250);
+	ctx.drawImage(userImage, 0, 0, 2048, 2048);
 	
 	ctx.rotate(-Math.PI / 4);
 	ctx.fillStyle = '#f00';
-	ctx.font = '130px RobotoMedium';
-	ctx.fillText('Yikes', -150, 250);
+	ctx.font = '1065px RobotoMedium';
+	ctx.fillText('Yikes', -1229, 2048);
 
 	message.channel.send({ files: [{ attachment: canvas.toBuffer(), name: 'yikes.png' }] });
 };
